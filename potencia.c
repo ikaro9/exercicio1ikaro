d982f529c5bad904165f29ec52971e13d055ea75
ikaro <ikarobrito722@gmail.com>
2025-04-26 21:19:57 -0300

    deixando a função mais simples

--- potencia.c
+++ potencia.c
@@ -2,20 +2,10 @@
 #include<locale.h>
 
 int potencia(int base, int expoente){
-    int temp = base;
     int resultado=1;
     int i;
-    if(expoente == 0){
-        resultado = 1;
-    }
-    if(expoente == 1){
-        resultado = base;
-    }
-    for(i = 2;i<=expoente;i++){
-        
-        resultado = base*temp;
-        base = resultado;
-       
+    for(i=0;i<expoente;i++){
+         resultado = resultado *base;
     }
     return resultado;
 }
