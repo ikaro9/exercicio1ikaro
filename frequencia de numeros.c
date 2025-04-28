281e2e138ef30f72252b577a3ff477003aca973e
ikaro <ikarobrito722@gmail.com>
2025-04-28 20:42:34 -0300

    corrigido bug de repetição de numero de vezes de um numero

--- frequencia de numeros.c
+++ frequencia de numeros.c
@@ -1,26 +1,44 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
+void frequencia(int *p)
+{
 
-void frequencia(int *p){
- 
-    for(int i=0;i<10;i++){
+    for (int i = 0; i < 10; i++)
+    {
         int vezes = 0;
-        for(int j=0;j<10;j++){
-        if(p[i] == p[j]){
-            vezes++;
-            printf("%d aparece %d vezes\n",p[i],vezes);
+        int contado = 0;
+
+        for (int k = 0; k < i; k++)
+        {
+            if (p[i] == p[k])
+            {
+                contado = 1;
+                break;
+            }
+        }
+        if (!contado)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if (p[i] == p[j])
+                {
+                    vezes++;
+                }
+            }
+
+            printf("%d aparece %d vezes\n", p[i], vezes);
         }
     }
-
-}
 }
 
-int main(){
+int main()
+{
     int vet[10];
     printf("Digite os números \n");
-    for(int i=0;i<10;i++){
-    scanf("%d",&vet[i]);
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d", &vet[i]);
     }
     frequencia(vet);
     return 0;
